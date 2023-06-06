ca4b69e7f074517c6d670ac869d2faa335f1d2a0
nihalDelimp <nihal@delimp.com>
2023-06-06 14:07:00 +0530

    Chnagessss

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package mongodbpackage
+package testPackageLogger
 
 import (
 	"log"
@@ -6,4 +6,12 @@ import (
 
 func LogInfo(message string) {
 	log.Printf("Info %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("Error %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("Warning %v", message)
 }
