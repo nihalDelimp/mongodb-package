540379480606d3b05d2cdd08723251fe64c36a73
nihalDelimp <nihal@delimp.com>
2023-06-06 13:50:37 +0530

    Removing some code

--- main.go
+++ main.go
@@ -1,43 +1,8 @@
 package mongodbpackage
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"time"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-func connectToMongoDB(dbType, host, port, dbUser, dbPwd, dbName, collectionName string) (*mongo.Client, *mongo.Collection, context.Context, error) {
-	// Construct MongoDB connection URI
-	mongodbURI := dbType + "://" + dbUser + ":" + dbPwd + "@" + host + ":" + port
-
-	// Configure the client connection
-	clientOptions := options.Client().ApplyURI(mongodbURI)
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.Background(), clientOptions)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to connect to MongoDB: %v", err)
-	}
-
-	// Check if the connection was successful
-	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to ping MongoDB: %v", err)
-	}
-
-	// Access the specified database and collection
-	db := client.Database(dbName)
-	collection := db.Collection(collectionName)
-
-	// Create a context with a 15-second timeout
-	ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
-
-	return client, collection, ctx, nil
-}
 
 func Logger(message string) {
 	log.Printf("Error %v", message)
